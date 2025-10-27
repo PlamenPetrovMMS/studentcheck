11c185fabfd022832058c8d5d81bf2e68d20a78e
PlamenPetrovMMS <flamepetrov01@gmail.com>
2025-10-27 14:25:36 +0200

    New

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!");
+document.getElementById('studentBtn').addEventListener('click', function() {
+    window.location.href = 'studentLogin.html';
+});
+document.getElementById('teacherBtn').addEventListener('click', function() {
+    window.location.href = 'teacherLogin.html';
+});
