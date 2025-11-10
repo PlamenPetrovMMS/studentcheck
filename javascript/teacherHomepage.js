dbd2992e8ce74c213832a95208b3a9bb63a117c6
PlamenPetrovMMS <flamepetrov01@gmail.com>
2025-11-10 14:20:58 +0200

    Update teacherHomepage.js

--- javascript/teacherHomepage.js
+++ javascript/teacherHomepage.js
@@ -1,7 +1,11 @@
 // Add click logging when user clicks an item in the UL with the addClassBtn inside teacherHomepage.html
 document.addEventListener('DOMContentLoaded', () => {
 	document.getElementById('addClassBtn')?.addEventListener('click', () => {
-        console.log('Add Class button clicked');
+        console.log("Add Class button clicked.");
+        var ul = document.getElementById("classList");
+        var li = document.createElement("li");
+        li.textContent = "New Class Item";
+        ul.appendChild(li);
     });
 });
 
