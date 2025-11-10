baff5c955dde61ece49d3e7c60b695ebab994988
PlamenPetrovMMS <flamepetrov01@gmail.com>
2025-11-10 15:18:52 +0200

    Update studentLogin.js

--- javascript/studentLogin.js
+++ javascript/studentLogin.js
@@ -53,10 +53,8 @@ document.getElementById('studentLoginForm').addEventListener('submit', async fun
             },
             body: JSON.stringify(studentData)
         });
-
         const t1 = performance.now();
         console.log(`Response received from server in ${Math.round(t1 - t0)} ms`);
-
         if (response.ok) {
             const data = await response.json();
             if(data.loginSuccess) {
