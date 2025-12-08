bb29a53db186ee439e2005d948c5d551cf265b9f
PlamenPetrovMMS <flamepetrov01@gmail.com>
2025-12-08 13:51:15 +0200

    Update teacherHomepage.js

---
+++ javascript/teacherHomepage.js
@@ -0,0 +1,9 @@
+
+
+
+// New Class button
+const addNewClassBtn = document.getElementById('addNewClassBtn');
+addNewClassBtn.addEventListener('click', () => {
+    const overlay = document.getElementById('createClassOverlay');
+    overlay.style.display = 'flex';
+});
